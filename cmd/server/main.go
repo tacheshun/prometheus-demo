ef7fd7dadfec6a134ad1f8ee4df99be58f66f42d
Marius Godeanu <marius.godeanu_costache@mail.schwarz>
2022-01-24 01:22:47 +0200

    added configs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/client_golang/prometheus/push"
+	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
 
 var addr = flag.String("listen-address", ":8081", "The address to listen on for HTTP requests.")
@@ -38,8 +37,33 @@ var (
 )
 
 func main() {
-
 	rand.Seed(time.Now().UnixNano())
+	r := gin.New()
+
+	p := ginprometheus.NewPrometheus("http")
+	p.Use(r)
+
+	r.GET("/", func(c *gin.Context) {
+
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+
+		switch rand.Intn(6) {
+		case 0:
+			c.JSON(200, "Hello world!")
+		case 1:
+			c.JSON(404, "Not Found!")
+		case 2:
+			c.JSON(500, "Oops!")
+		case 3:
+			c.JSON(401, "Unauthorized!")
+		case 4:
+			c.JSON(403, "Forbidden!")
+		case 5:
+			c.JSON(408, "Timeout!")
+		default:
+			c.JSON(200, "Hello world!")
+		}
+	})
 
 	go func() {
 		for {
@@ -47,20 +71,10 @@ func main() {
 			h.Observe(float64(rand.Intn(100-0+1) + 0))
 			d.With(prometheus.Labels{"device": "/dev/sda"}).Inc()
 			c.Inc()
+			fmt.Print(".")
 			time.Sleep(1 * time.Second)
 		}
 	}()
 
-	go func() {
-		for {
-			// Example of push metric
-			_ = push.New("http://pushgateway:9091", "skeleton_job").Collector(e).Add()
-			e.Inc()
-			fmt.Print("_")
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	log.Fatal(r.Run(*addr))
 }
